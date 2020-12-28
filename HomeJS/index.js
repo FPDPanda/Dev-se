350fbb9e9ddf52edc4992fbce9a70553d4f46528
vitoivan <viictor.ivan@gmail.com>
2020-12-28 18:08:04 -0300

    Melhorando aparencia e fazendo site ficar responsivo

---
+++ HomeJS/index.js
@@ -0,0 +1,5 @@
+function openMenu(){
+    document.body.classList.toggle('hiddenof')
+    document.getElementById('hamburger').classList.toggle('clicked')
+    document.getElementById('hamb-navbar').classList.toggle('clicked')
+}
