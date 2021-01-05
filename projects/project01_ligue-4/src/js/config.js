1543e808546d5cb20a5167412dce321e585263de
FPDPanda <jeanjacques7.jj@gmail.com>
2021-01-05 17:28:00 -0300

    Formatado com prettier

--- projects/project01_ligue-4/src/js/config.js
+++ projects/project01_ligue-4/src/js/config.js
@@ -1,14 +1,14 @@
-const CONFIG={
-    parentEl:document.querySelector("#ul"),//TAG PAI QUE POSSUI TODAS AS COLUNAS, OU SEJA, AS 7 COLUNAS
-    allColumns:document.querySelectorAll(".lis"),//PASSAR TODAS AS COLUNAS
-    pipe:document.querySelector("#pipe"),//Esse é o cano
-    btnReset:document.querySelector("#btnReset"),
-    ball:{
-        element:"div",
-        nameClass:"ball",
-        toggleColor:["rgb(0,25,255)","yellow"],//Aqui pode configurar as cores que desejar para a bolinha
-        timeToAppear:.10,//Aqui configura os segundos que a bolinha irá aparecer.
-        fallSpeed:10//Configura a velocidade da queda
-    }
-}
-new Controller(CONFIG)//TUDO COMEÇA
+const CONFIG = {
+  parentEl: document.querySelector("#ul"), //TAG PAI QUE POSSUI TODAS AS COLUNAS, OU SEJA, AS 7 COLUNAS
+  allColumns: document.querySelectorAll(".lis"), //PASSAR TODAS AS COLUNAS
+  pipe: document.querySelector("#pipe"), //Esse é o cano
+  btnReset: document.querySelector("#btnReset"),
+  ball: {
+    element: "div",
+    nameClass: "ball",
+    toggleColor: ["rgb(0,25,255)", "yellow"], //Aqui pode configurar as cores que desejar para a bolinha
+    timeToAppear: 0.1, //Aqui configura os segundos que a bolinha irá aparecer.
+    fallSpeed: 10, //Configura a velocidade da queda
+  },
+};
+new Controller(CONFIG); //TUDO COMEÇA
